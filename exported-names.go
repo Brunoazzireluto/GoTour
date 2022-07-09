c1943de6227e554166b996aa7e50ceba5052ef42
Bruno Alves <bruno.a@outlook.com.br>
2022-07-09 13:19:10 -0300

    adding first files
    adding firts files for GoTour

---
+++ exported-names.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// fmt.Println(math.pi)  código errado
+	fmt.Println(math.Pi) //o Nome exportado certo deve sempre comeaçar com Maiuscula.
+}
---
+++ imports.go
@@ -0,0 +1,16 @@
+// Este grupo de códigos em parênteses da importação, é a declaração de importação "consignada". Você também pode escrever várias declarações de importação assim:
+
+// import "fmt"
+// import "math"
+// Mas é um bom estilo usar a declaração de importação consignada.
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("Agora você tem %g problemas. \n", math.Sqrt(7))
+}
